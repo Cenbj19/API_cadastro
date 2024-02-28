a4f9f67b74ccead5ec649ee7c6e18e645d437075
cenbj19 <carloseduardo.junior@hotmail.com>
2024-02-28 11:19:05 -0300

    Projeto criado

---
+++ CadastroClientess/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CadastroClientess.Models;
+
+namespace CadastroClientess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")] // post estou enviando/postando o serviço para API
+        public object Salvar([FromBody] Clientes clientes)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            { 
+            
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CadastroClientess/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿namespace CadastroClientess.Models
+{
+    public class Clientes
+    {
+        // Criando atributos para nossos clientes
+        // get pegar informação / set adicionar informação
+        public int IdCliente { get; set; }
+        public string Documento { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Fax { get; set; }
+        public string UF { get; set; }
+    }
+}
